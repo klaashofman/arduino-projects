4716952842e2a75175ab3b617669c10c2abfced4
Klaas Hofman <klaas@mind.be>
2014-12-18 10:19:05 +0100

    don't remember what I did, sorry

--- bluetooth_data_logger/bluetooth_data_logger.ino
+++ bluetooth_data_logger/bluetooth_data_logger.ino
@@ -1,28 +1,28 @@
 /*
 Arduino pins
-Digital:
-0: serial RX 
-1: serial TX
-2: bluetooth RX
-3: bluetooth TX
-4:  
-5: ~PWM
-6: ~PWM
-7:
-8: SDD shielf LED1
-9: SDD shield LED2
-10: ~PWM: SD CS
-11: ~PWM: SD MOSI
-12: SD MISO
-13: SD CLK
-Analog
-0: SENSOR1: temp
-1: SENSOR2: LDR
-2: SENSOR3
-3: SENSOR4:
-4: SDA/SCL RTC
-5: SDA/SCL RTC
-*/
+ Digital:
+ 0: serial RX 
+ 1: serial TX
+ 2: bluetooth RX
+ 3: bluetooth TX
+ 4:  
+ 5: ~PWM
+ 6: ~PWM
+ 7:
+ 8: SDD shielf LED1
+ 9: SDD shield LED2
+ 10: ~PWM: SD CS
+ 11: ~PWM: SD MOSI
+ 12: SD MISO
+ 13: SD CLK
+ Analog
+ 0: SENSOR1: temp
+ 1: SENSOR2: LDR
+ 2: SENSOR3
+ 3: SENSOR4:
+ 4: SDA/SCL RTC
+ 5: SDA/SCL RTC
+ */
 
 #include <Wire.h>
 #include <RTClib.h>
@@ -44,10 +44,7 @@ SoftwareSerial bluetooth(rxBt, txBt);
 const int SD_chip_select = 10;
 const int led_green = 8;
 const int led_red = 9;
-File dataFile; 
-
-const int block_size = 2048;
-unsigned char block_buf[block_size];
+File dataFile;
 
 // RTC
 RTC_DS1307 RTC;
@@ -59,50 +56,50 @@ static void setup_bt(){
 }
 
 static void setup_led() {
-   pinMode(led_green, OUTPUT);
-   pinMode(led_red, OUTPUT); 
+  pinMode(led_green, OUTPUT);
+  pinMode(led_red, OUTPUT); 
 }
 
 static void setup_rtc() {
-   Wire.begin();
-   RTC.begin();
-   if (! RTC.isrunning()) {
+  Wire.begin();
+  RTC.begin();
+  if (! RTC.isrunning()) {
     Serial.println("RTC is NOT running!");
     // following line sets the RTC to the date & time this sketch was compiled
     // uncomment it & upload to set the time, date and start run the RTC!
     RTC.adjust(DateTime(__DATE__, __TIME__));
-   }  
+  }  
 }
 
 static int setup_sd(){
- // make sure that the default chip select pin is set to
-   // output, even if you don't use it:
-    pinMode(SD_chip_select, OUTPUT);
-    // see if the card is present and can be initialized:
-    if (!SD.begin(SD_chip_select)) {
-      Serial.println("Card failed, or not present");
-      return 0;
-    }
-    Serial.println("card initialized.");
-    return 1;
+  // make sure that the default chip select pin is set to
+  // output, even if you don't use it:
+  pinMode(SD_chip_select, OUTPUT);
+  // see if the card is present and can be initialized:
+  if (!SD.begin(SD_chip_select)) {
+    Serial.println("Card failed, or not present");
+    return 0;
+  }
+  Serial.println("card initialized.");
+  return 1;
 }
 
 void setup(){
-   setup_bt(); 
-   Serial.begin(38400);
-   setup_led();
-   setup_rtc();
-   if (!setup_sd()) {
-     return;
-   }
-   Serial.println("Bluetooth data logger");
+  setup_bt(); 
+  Serial.begin(38400);
+  setup_led();
+  setup_rtc();
+  if (!setup_sd()) {
+    return;
+  }
+  Serial.println("Bluetooth data logger");
 }
 
 static void toggle_led(int led)
 {
-//  int s = digitalRead(led);
-//  s ^= 1;
-//  digitalWrite(led, s);
+  //  int s = digitalRead(led);
+  //  s ^= 1;
+  //  digitalWrite(led, s);
   digitalWrite(led, digitalRead(led) == 0 ? 1 : 0);
 }
 
@@ -144,7 +141,8 @@ static void process_cmd(const char *c, int bt)
     if (bt) {
       bluetooth.print("Boinkers BT:");
       bluetooth.println(get_fmt_date());
-    } else {
+    } 
+    else {
       Serial.println("Boinkers SR:");
       Serial.println(get_fmt_date());
     }
@@ -154,64 +152,56 @@ static void process_cmd(const char *c, int bt)
       bluetooth.print(sensor.humidity);
       bluetooth.print(",");
       bluetooth.println(sensor.temperature);
-    } else {
+    } 
+    else {
       Serial.print(sensor.humidity);
       Serial.print(",");
       Serial.println(sensor.temperature);
     }
   }
-//  if (cmd == "FILE"){
-//    String resp;
-//    if (dataFile){
-//      resp = String(dataFile.size(), DEC); // total dataFile size in bytes
-//      resp += " ";
-//      resp += String(block_size, DEC); // size of one block
-//      resp += " ";
-//      resp += String((dataFile.size() / block_size) + 1); // #blocks to copy + 1
-//    } else {
-//      resp = "File not found";
-//    }
-//  } // FILE
-//  if (cmd == "UPLOAD"){
-//    int block = 0;
-//    const int total_block = (dataFile.size() / block_size) + 1; 
-//    while (block < total_block){
-//      memset(block_buf, 0, block_size);
-//      if ( !dataFile.available() ){
-//        Serial.println("oops, dataFile ended primaturely");
-//        return;
-//      }
-//      if (dataFile.readBytes((char *)block_buf, block_size)){
-//        if (!bluetooth.write(block_buf, block_size)) {
-//          Serial.print("write bytes to bluetooth returned 0 on block");
-//          Serial.println(block);
-//          return;  
-//        }
-//      } else { 
-//        Serial.print("readbytes on dataFile returned 0 on block:");
-//        Serial.println(block);
-//        return;
-//      }
-//      Serial.print("uploading block: ");
-//      Serial.print(block, DEC);
-//      Serial.print("of total: ");
-//      Serial.println(total_block, DEC);
-//      block++;
-//    } // while
-//  } // UPLOAD
-//  if (cmd == "LOG")
-//  {
-//     if (bt) {
-//       while(dataFile.available()) {
-//         bluetooth.write(dataFile.read());
-//       }
-//       // dataFile.close();
-//     } else {
-//       while(dataFile.available()) {
-//         Serial.write(dataFile.read());
-//       }
-//     }
-//  } // LOG
+  if (cmd == "FILE"){
+    String resp;
+    dataFile = SD.open("datalog.txt", FILE_READ);
+    if (dataFile){
+      resp = String(dataFile.size(), DEC); // total dataFile size in bytes
+      dataFile.close();
+    } 
+    else {
+      resp = "File not found";
+    }
+    if (bt){
+      bluetooth.println(resp);
+    } 
+    else {
+      Serial.println(resp);
+    }
+  } // FILE
+  if (cmd == "UPLOAD"){
+    dataFile = SD.open("datalog.txt", FILE_READ);
+    if (dataFile) {
+      while (dataFile.available()){
+        bluetooth.write(dataFile.read());
+      }
+      bluetooth.println("EOF");
+      dataFile.close();
+    } // data file
+    else {
+      Serial.println("Unable to open file");
+    }  
+  } // UPLOAD
+  //  if (cmd == "LOG")
+  //  {
+  //     if (bt) {
+  //       while(dataFile.available()) {
+  //         bluetooth.write(dataFile.read());
+  //       }
+  //       // dataFile.close();
+  //     } else {
+  //       while(dataFile.available()) {
+  //         Serial.write(dataFile.read());
+  //       }
+  //     }
+  //  } // LOG
 }
 
 static int tick = 0;
@@ -231,13 +221,14 @@ void loop(){
     if (dataFile) {
       dataFile.println(status);
       dataFile.close();
-      delay(10);
-    } else {
-      Serial.println("error opening datalog.txt");
+    } 
+    else {
+      Serial.println("error writing datalog.txt");
     }
   } // tick
-  
-  char c[32] = {0};
+
+  char c[32] = {
+    0  };
   int i = 0;
   while (bluetooth.available()){
     c[i++] = bluetooth.read();
@@ -253,8 +244,9 @@ void loop(){
   if (i > 0) {
     process_cmd(c, 0);
   }
- delay(10);
- toggle_led(led_green);
- tick++; 
+  delay(10);
+  toggle_led(led_green);
+  tick++; 
 }
+
 
