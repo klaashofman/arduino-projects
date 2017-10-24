a5c6949248eb7448ca3b4b87f5daf765a667542d
Klaas Hofman <klaas.hofman@gmail.com>
2017-10-24 10:12:08 +0200

    scroll through a list of options on the LCD screen

---
+++ teensy_encoder_lcd_select_simple/teensy_encoder_lcd_select_simple.ino
@@ -0,0 +1,82 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define ENCODER_USE_INTERRUPTS
+#include <Encoder.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define PIN_ENC_DATA  31
+#define PIN_ENC_CLK   32
+#define PIN_ENC_SW    33 
+
+Encoder myEnc(PIN_ENC_DATA, PIN_ENC_CLK);
+
+volatile int selected = 0;
+const char *list[] = { "one", "two", "three" };
+
+void setup() {
+  /* setup uart */
+  Serial.begin(9600);
+  Serial.println("Simple encoder select test");
+
+  /* setup encoder pins */
+  pinMode(PIN_ENC_DATA, INPUT_PULLUP);
+  pinMode(PIN_ENC_CLK, INPUT_PULLUP);
+  pinMode(PIN_ENC_SW, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PIN_ENC_SW), isr_select, FALLING);
+  
+  /* setup lcd */
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  display.display();
+  delay(2000);
+
+  // Clear the buffer.
+  display.clearDisplay();
+  display.setTextSize(1);
+  
+  for (int i=0 ; i < 3 ; i++){
+    display.println(list[i]);
+  }
+  display.display();
+}
+
+long oldPosition  = -999;
+
+void update_list(int s){
+  for (int i=0 ; i < 3 ; i++){
+    if (i == s)
+      display.setTextColor(BLACK, WHITE); // 'inverted' text
+     else
+      display.setTextColor(WHITE);
+      
+    display.println(list[i]);
+  }
+  display.display();
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  long newPosition = myEnc.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+    if (newPosition > oldPosition)
+      selected++;
+    else
+      selected--;
+    selected %= 3;
+    update_list(selected);
+  }
+}
+
+void isr_select(){
+  // code when rotary knob is pressed
+  Serial.print("selected: ");
+  Serial.println(selected, DEC);
+}
+
